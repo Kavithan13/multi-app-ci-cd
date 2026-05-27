6fe0dced119e07c638bfacbc59dcdcebdaf085b1
Kavithan13 <nagamkavitha1999@gmail.com>
2026-05-27 13:11:00 +0530

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", (HttpExchange exchange) -> {
+            String response = "Hello from Simple Java App!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+}
